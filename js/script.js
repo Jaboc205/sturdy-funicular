ab75accc3dc9ab75e5002e33b54749e8e1791a06
Jake <jacob.caissie@gmail.com>
2025-08-17 13:54:16 -0700

    Played with the calculator, got it to finally add numbers and not strings :P I still havent made it to work with subsequent operations without clearing, and need to work on the other operators as well.

--- js/script.js
+++ js/script.js
@@ -23,18 +23,101 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+// Calculator variables
+let firstValue = 0;
+let secondValue = 0;
+let operation = null;
+
+
 // Calculator Functions
 function addToCalc(num) {
   const existingNum = document.getElementById('main-display');
-  console.log(existingNum.textContent + " + " + num);
-  if (existingNum.textContent === "0") {
-    existingNum.textContent = num;
+  let updateNum = null;
+  if (operation == null) {
+    // add to first number
+    if (firstValue == 0) {
+      updateNum = `${num}`;
+      firstValue = updateNum;
+      existingNum.textContent = updateNum;
+    } else {
+      updateNum = `${firstValue}${num}`;
+      firstValue = updateNum;
+      existingNum.textContent = updateNum;
+    }
   } else {
-    existingNum.textContent += num;
+    if (secondValue == 0) {
+      updateNum = `${num}`;
+      secondValue = updateNum;
+      existingNum.textContent = updateNum;
+    } else {
+      updateNum = `${secondValue}${num}`;
+      secondValue = updateNum;
+      existingNum.textContent = updateNum;
+    }
   }
+  
+  
+  console.log(`
+    updateNum: ${updateNum}
+    num1:  ${firstValue}
+    num2: ${secondValue}
+    OP:  ${operation}
+  `);
+
+  // if (operation == null) {
+  //   if (existingNum.textContent === "0") {
+  //   
+  //   storedValue = num;
+  //   } else {
+  //   existingNum.textContent += num;
+  //   storedValue += num;
+  //   }
+  // } else if (operationValue == 0) {
+  //   existingNum.textContent = num;
+  //   operationValue = num; 
+  // } else {
+  //   existingNum.textContent += num;
+  //   operationValue += num; 
+  // }
+  
 };
 
 function clearCalc() {
+  operation = null;
+  firstValue = 0;
+  secondValue = 0;
   document.getElementById('main-display').textContent = 0;
+  console.log(`
+    num1:  ${firstValue}
+    num2: ${secondValue}
+    OP:  ${operation}  
+  `);
   console.log("Calculator cleared");
 };
+
+function operator(op) {
+  const display = document.getElementById('main-display');
+  display.textContent += ` ${op} `;
+  operation = op;
+  console.log(`
+    num1:  ${firstValue}
+    num2: ${secondValue}
+    OP:  ${operation}   
+  `);
+}
+
+function equalsCalc() {
+  console.log("equals");
+  if (operation == "+") {
+    console.log(firstValue + "=" + firstValue + "+" + secondValue);
+    sum =  Number(firstValue) + Number(secondValue);
+    document.getElementById('main-display').textContent = sum;
+    storedValue = sum
+  }
+  console.log(`
+    num1:  ${firstValue}
+    num2: ${secondValue}
+    OP:  ${operation}
+    sum: ${sum}
+  `);
+}
